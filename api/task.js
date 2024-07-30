8ca50690ea5f331598650a76eb75adf8ef9e03c0
Merik <merikreyimov@gmail.com>
2024-07-30 07:34:11 +0300

    task api added

---
+++ api/task.js
@@ -0,0 +1,65 @@
+import { supabase } from './config';
+
+export async function getTasks(email, password) {
+  let { data, error } = await supabase.from('tasks').select('*');
+
+  if (error) {
+    throw Error(error?.message || 'Что то случилось при получении задач');
+  }
+
+  return data;
+}
+
+export async function getTask(taskId) {
+  
+  const { data, error } = await supabase
+  .from('tasks')
+  .select()
+  .eq('id', taskId)
+  .single();
+
+  if (error) {
+    throw Error(error?.message || 'Что то случилось при получении задачи');
+  }
+
+  return data;
+}
+
+export async function createTask(taskData) {
+ 
+  const { data, error } = await supabase.from('tasks').insert(taskData).select();
+        
+
+  if (error) {
+    throw Error(error?.message || 'Что то случилось при создании задачи');
+  }
+
+  return data;
+}
+
+export async function updateTask(taskData) {
+  const { data, error } = await supabase
+  .from('tasks')
+  .update(taskData)
+  .eq('id', taskData.id)
+  .select();
+        
+
+  if (error) {
+    throw Error(error?.message || 'Что то случилось при обнавлении  задачи');
+  }
+
+  return data;
+}
+
+export async function deleteTask(taskId) {
+  const { error } = await supabase
+  .from('tasks')
+  .delete()
+  .eq('id', taskId);
+        
+
+  if (error) {
+    throw Error(error?.message || 'Что то случилось при удалении задачи');
+  }
+}
